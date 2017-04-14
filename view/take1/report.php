9652646e133a47b3435ce6925934c9d667dc0554
Emil Sandberg <emil.hietanen@gmail.com>
2017-04-14 15:07:59 +0200

    Added report kmom01

--- view/take1/report.php
+++ view/take1/report.php
@@ -1,8 +1,33 @@
 <h1>Redovisningstexter</h1>
 
 <h2>Kursmoment 01</h2>
+<h3>Allmänna reflektioner:</h3>
 <p>
-    Redovisningstext...
+    Det här var ett massivt första moment. Jag upplever att här fanns tillräckligt med material för två kursmoment, i alla fall tog det mig ungefär så mycket tid att komma igenom allt. Det blev lite överväldigande när momentet omfattade såväl oop som ramverk och även lite sql. Jag tycker att man borde ha prioriterat lite och fokuserat på två av dessa delar och inte alla tre.
+</p>
+<p>
+    Trots att kursmomentet var massivt så saknade jag läsanvisningar. Jag kikade på ett par kommande kursmoment och märkte att det var skralt med ordentliga läsanvisningar även framöver, vilket jag tycker är synd. Jag har uppskattat kursböckerna vi haft tidigare och har lärt mig mycket av dem, eftersom de ofta förklarar ämnena lite annorlunda än skolans artiklar. Finns det verkligen så dåligt med kursböcker om oop i PHP eller vad är orsaken bakom detta?
+</p>
+<p>
+    Det gick också åt tid att tolka några av uppgifterna som var lite otydligt formulerade, närmare bestämt Gissa numret- och navbar-uppgifterna. Gissa numret-uppgiften krävde ganska många genomläsningar för att förstå alla kraven och en del var tolkningsbart. Det finns till exempel en mening i reglerna som lyder "Klassen skall innehålla en metod som gör en gissning" och på skärmbilderna finns det en randomize-knapp. Detta kan tolkas som att Guess-klassen ska göra en automatisk slumpmässig gissning, eller att klassen ska hantera användarens gissning. Jag ser inte vad det förra fallet skulle bevisa så jag implementerade endast det senare fallet. Det är en sak om uppgifterna är öppna och ger utrymme för oss att skapa våra egna implementeringar, men i dessa fall fanns det instruktioner men de var otydliga.
+</p>
+
+<h3>Hur känns det att hoppa rakt in i klasser med PHP, gick det bra?</h3>
+<p>
+    Vi har ju farten uppe från oopython-kursen så det gick ganska smidigt att flytta över tänket till PHP-språket. Att leta i referensmanualen för PHP går också väldigt bra numera, tidigare har jag funnit den lite otymplig. Jag förstår att den här kursen ska utmana oss lite mera och att vi ska få leta efter informationen vi behöver lite mer på egen hand. Till exempel så förklaras aldrig vad namespace är för något, bara att man ska använda det, så det fick jag kolla upp själv. Detta är inget problem men jag önskar att man kunde ha nämnt kursstrategin inledningsvis (jag hoppas att det är ett medvetet val och inte missar i materialet).
+</p>
+<p>
+    Jag har länkat till Guess-spelet från about-sidan i min me-sida.
+</p>
+
+<h3>Berätta om dina reflektioner kring ramverk, anax-lite och din me-sida.</h3>
+<p>
+    Det var mycket lärorikt att lära sig skapa ett "eget" ramverk, jag fick en mycket bättre förståelse för vilka olika delar som behövs för att få ett ramverk att fungera. Nu var det ju väldigt mycket som vi bara laddade ner och satte in så det finns en känsla av att jag inte har full kontroll över koden som styr min sida. Dock var ingen av delarna oöverkomligt stora och jag skummade igenom det som laddades ned så jag i alla fall hade ett hum om vad modulerna innehöll. Så i detta skede känner jag att jag en ganska god förståelse för ramverkets fördelar och begränsningar. Nu gör jag ju denna kurs parallellt med design-kursen så jag vet inte riktigt allt vad anax-flat innehåller och vissa element som CImage får jag hoppa över.
+</p>
+
+<h3>Gick det bra att komma igång med MySQL, har du liknande erfarenheter sedan tidigare?</h3>
+<p>
+    Det är första gången jag använder MySQL men i tidigare kurser (oopython, htmlphp) har SQL introducerats genom SQLite, så egentligen var det ju inget nytt i sig. Jag har svårt att se skillnaderna mellan SQLite och MySQL/MariaDB, det enda jag kan se just nu är att databasen inte existerar som fil på min dator i MySQL, utan jag antar att den ligger på MySQL-servern jag dragit igång via XAMPP. Det var en hel del att gå igenom även i SQL-biten. Först var det installationen och de olika MySQL-klienterna och även hur man använder MySQL på studentservern. Sedan var det SQL-uppgiften, som verkligen var massiv så det var tur att den var uppdelad i tre delar. Jag hade lagt så mycket tid på kursmomentet hittills så jag gjorde endast avsnitt 1-6 av uppgiften. Av det var det mesta bekant från tidigare men kommandot ALTER var intressant och väldigt praktiskt. Jag tyckte ganska mycket om MySQL CLU men använder ändå Workbench i uppgiften.
 </p>
 
 <h2>Kursmoment 02</h2>
